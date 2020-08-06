e104bd68c4847714d050fad80f2c1c353bbc3a2c
arnaud <arnaud@idelub.com>
2020-08-06 12:30:12 +0200

    adding container to Signing as return value

---
+++ lib/widgets/signing_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SigningWidget extends StatefulWidget {
+  @override
+  _SigningWidgetState createState() => _SigningWidgetState();
+}
+
+class _SigningWidgetState extends State<SigningWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
